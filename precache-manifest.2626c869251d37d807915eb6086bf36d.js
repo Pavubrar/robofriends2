ebc5d36d3b63d680cbdfda47995239b8ddce0aac
Pavubrar <56096198+Pavubrar@users.noreply.github.com>
2020-02-08 11:16:25 -0800

    Updates

---
+++ precache-manifest.2626c869251d37d807915eb6086bf36d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cd475729d9d396d45a771d90b4672b4",
+    "url": "/robofriends2/index.html"
+  },
+  {
+    "revision": "49243e08355c81b6dca1",
+    "url": "/robofriends2/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "64cd445919fcaeeb4e0c",
+    "url": "/robofriends2/static/css/main.72cc5d6e.chunk.css"
+  },
+  {
+    "revision": "49243e08355c81b6dca1",
+    "url": "/robofriends2/static/js/2.6e677d60.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robofriends2/static/js/2.6e677d60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64cd445919fcaeeb4e0c",
+    "url": "/robofriends2/static/js/main.a91d4b2f.chunk.js"
+  },
+  {
+    "revision": "bd63112e2ccc7cb01888",
+    "url": "/robofriends2/static/js/runtime-main.516e87c8.js"
+  },
+  {
+    "revision": "39235ed81e87e319136e7cfee7392e49",
+    "url": "/robofriends2/static/media/Fontarian Sparkling.39235ed8.woff"
+  },
+  {
+    "revision": "d25dc1ca53ce75b389439588c2843bcc",
+    "url": "/robofriends2/static/media/Fontarian.d25dc1ca.woff"
+  }
+]);
